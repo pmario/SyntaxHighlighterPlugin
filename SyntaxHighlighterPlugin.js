f9cb31cf26628068d635e920a59c0e430f7a4c4d
pmario <mp@pep-t.net>
2010-08-24 23:01:29 +0200

    V 0.2.0 changed concept. added new <code> formatter.

--- SyntaxHighlighterPlugin.js
+++ SyntaxHighlighterPlugin.js
@@ -2,8 +2,8 @@
 |''Name''|SyntaxHighlighterPlugin3|
 |''Description''|Enables syntax highlighting|
 |''Author''|PMario|
-|''Version''|0.1.0|
-|''Status''|@@experimental@@|
+|''Version''|0.2.0|
+|''Status''|''beta''|
 |''Source''|http://syntaxhighlighter.tiddlyspace.com/#SyntaxHighlighterPlugin3|
 |''License''|[[BSD|http://www.opensource.org/licenses/bsd-license.php]]|
 |''CoreVersion''|2.5.0|
@@ -12,15 +12,8 @@
 !Documentation
 *see: [[SyntaxHighlighterPlugin3Info]]
 !Description
-Enables syntax highlighting for <pre> blocks.
+Enables syntax highlighting for <pre> and <code> blocks. Adds a new formatter for {{{<code class='brush:???'>}}} 
 !Usage
-!!!!Macro
-<<<
-*The macro will only render the containing tiddler, when viewed.
-{{{
-<<highlightSyntax>>
-}}}
-<<<
 !!!!StyleSheet
 <<<
 *add this to your StyleSheet
@@ -29,51 +22,63 @@ Enables syntax highlighting for <pre> blocks.
 [[ShThemeDefault.css]]
 }}}
 <<<
+!!!!Macro
+<<<
+*The macro is only needed if you have inline html blocks. see: [[SyntaxHighlighterPlugin3Info]]
+<<<
 !!!!ViewTemplate
 <<<
-<!--{{{-->
-<div class='tagging' macro='tagging'></div>
-<div macro='highlightSyntax'></div>  <!-- insert this line -->
-<div class='tagClear'></div>
-<!--}}}-->
+*Same as macro, but will be executed automatically for every tiddler. see: [[SyntaxHighlighterPlugin3Info]]
 <<<
 !!!!Parameters
 <<<
-<...>
+{{{<<highlightSyntax [tagName]>> }}}
+*will render all blocks, with any defined tag name. eg: tagName = code.
+*[tagName] is optional. Default is "pre".
+<<<
+!!!!Configuration options
+<<<
+Guess syntax: <<option chkGuessSyntax>> .. If activated, ~TiddlyWiky <pre> blocks will be rendered according to there block braces. see [[SyntaxHighlighterPlugin3Info]]
+Expert mode: <<option chkExpertSyntax>> .. If activated, additional values below will be used. see [[SyntaxHighlighterPlugin3Info]]
+
+{{{ {{{ }}} txtShText: <<option txtShText>> eg: 'brush:text tab-size:4 + options'
+{{{ /*{{{* / }}} txtShCss: <<option txtShCss>> eg: 'brush:css  + options'
+{{{ //{{{ }}} txtShPlugin: <<option txtShPlugin>> 'brush:js  + options'
+{{{ <!--{{{-->> }}} txtShXml: <<option txtShXml>> 'brush:xml  + options'
+
+Additional options can be found at: [[SyntaxHighlighter homepage|http://alexgorbatchev.com/SyntaxHighlighter/manual/configuration/]]
 <<<
 !!!!Revision History
 <<<
-*V 0.1.0 2010-08-17
-**initial release
+*V 0.2.0 2010-08-22
+**New formatter for {{{<code class='brush:???'>}}} is available now
+**expert mode uses config options now
 <<<
 !!!!ToDo
 <<<
-*Find a solutio for expter mode!
-*Find the problem with highlighting the regExp. (see "blue" code below)
+*
 <<<
 !!!Code
 ***/
 
 //{{{
-version.extensions.SyntaxHighlighterPlugin3 = {major: 0, minor: 1, revision: 0, date: new Date(2010,8,17)};
+version.extensions.SyntaxHighlighterPlugin3 = {major: 0, minor: 2, revision: 0, date: new Date(2010,8,22)};
 
 (function($) {
 
 if(!window.SyntaxHighlighter) {
-//	throw "Missing dependency: SyntaxHighlighter";
-	displayMessage("Missing dependency: SyntaxHighlighter");
+	throw "Missing dependency: SyntaxHighlighter";
 }
 
 config.macros.highlightSyntax = {
 	getElementsByClass: function (searchClass,node,tag) {
-		var classElements = new Array();
-		if ( node == null )
-			node = document;
-		if ( tag == null )
-			tag = '*';
+		var classElements = [];
+        if ( node == null ) node = document;
+        if ( tag == null )  tag = '*';
+		
 		var els = node.getElementsByTagName(tag);
 		var elsLen = els.length;
-		var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+		var pattern = new RegExp("(^|\\s)"+searchClass+"(:|\\s|$)");
 		for (i = 0, j = 0; i < elsLen; i++) {
 			if ( pattern.test(els[i].className) ) {
 				classElements[j] = els[i];
@@ -84,7 +89,9 @@ config.macros.highlightSyntax = {
 	},
 	
 	handler: function(place, macroName, params, wikifier, paramString, tiddler) {
-		var arr = this.getElementsByClass('sh', story.findContainingTiddler(place), 'pre');
+		// the configured tagName can be temporarily overwritten by the macro.
+		var tagName = params[0] || SyntaxHighlighter.config.tagName;
+		var arr = this.getElementsByClass('brush', story.findContainingTiddler(place), tagName);
 		for (i=0; i<arr.length; i++) {
 			SyntaxHighlighter.highlight(null, arr[i]);
 		}			
@@ -93,120 +100,72 @@ config.macros.highlightSyntax = {
 
 })(jQuery);
 //}}}
-
+/***
+!!!!!New formatter for {{{<code class='brush:??'>}}}
+***/
+//{{{
+config.formatters.push({
+	name: "highlightSyntax",
+	match: "^<code[\\s]+[^>]+>\\n",
+	element: "pre",
+	handler: function(w)
+	{
+        this.lookaheadRegExp = /<code[\s]+class.*=.*["'](.*)["'].*>\n((?:^[^\n]*\n)+?)(^<\/code>$\n?)/img;
+		this.lookaheadRegExp.lastIndex = w.matchStart;
+		var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
+		if(lookaheadMatch && lookaheadMatch.index == w.matchStart) {
+            var options = lookaheadMatch[1];
+			var text = lookaheadMatch[2];
+			if(config.browser.isIE)
+				text = text.replace(/\n/g,"\r");
+			var element = createTiddlyElement(w.output,this.element,null,options,text);
+            SyntaxHighlighter.highlight(null, element);
+			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
+		}
+	}
+});
+//}}}
 /***
 !!!!!Add class attribute to pre, if defined
 ***/
 //{{{
 (function(formatters) { //# set up alias
-	var expert = false;		// if true, the new regExp will be activated
-	var guess = true;	// guess language for syntax highlighting
-
 	var helper = {};	
-	helper.enclosedTextHelper = function(w, attr){
+	helper.enclosedTextHelper = function(w){
+		var attr;
+		var co = config.options;
+		var expert = (co.chkExpertSyntax != undefined)? co.chkExpertSyntax : false;
+		var guess  = (co.chkGuessSyntax != undefined)? co.chkGuessSyntax : true;
+		
 		this.lookaheadRegExp.lastIndex = w.matchStart;
 		var lookaheadMatch = this.lookaheadRegExp.exec(w.source);
 		if(lookaheadMatch && lookaheadMatch.index == w.matchStart) {
 			var text = lookaheadMatch[1];
 			if(config.browser.isIE)
 				text = text.replace(/\n/g,"\r");
-				
-			// check, if there is a class parameter. introduced for syntax highlighting.
-			if (!attr || attr == '') {
-				createTiddlyElement(w.output,this.element,null,null,text);				
-			} 
-			else {
-				createTiddlyElement(w.output,this.element,null,attr,text);				
+
+			switch(w.matchText) {
+			case "{{{\n": // text
+				attr = (expert) ? (co.txtShText) ? (co.txtShText) : 'brush:text' : 'brush:text' ;
+				break;
+			case "/*{{{*/\n": // CSS
+				attr = (expert) ? (co.txtShCss) ? (co.txtShCss) : 'brush:css' : 'brush:css';
+				break;
+			case "//{{{\n": // plugin
+				attr = (expert) ? (co.txtShPlugin) ? (co.txtShPlugin) : 'brush:js' : 'brush:js';
+				break;
+			case "<!--{{{-->\n": //template
+				attr =  (expert) ? (co.txtShXml) ? (co.txtShXml) : 'brush:xml' : 'brush:xml';
+				break;
 			}
+			var element = createTiddlyElement(w.output,this.element,null,attr,text);		
+	        if (guess || expert) SyntaxHighlighter.highlight(null, element);
+
 			w.nextMatch = lookaheadMatch.index + lookaheadMatch[0].length;
 		}
 	};
 	// merge the new helper function into formatterHelpers. 
 	merge(config.formatterHelpers, helper);
-	
-//}}}
-/***
-!!!!!Change formatter: "monospacedByLine
-***/
-//{{{
-	
-	var mono = formatters[formatters.findByField("name", "monospacedByLine")];
-
-	mono.element = "pre";
-
-	if (expert) {
-		mono.match = "^(?:/\\*\\{\\{\\{.*\\*/|\\{\\{\\{.*|//\\{\\{\\{.*|<!--\\{\\{\\{.*-->)\\n"; // new
-	}
-	else {
- 		mono.match = "^(?:/\\*\\{\\{\\{\\*/|\\{\\{\\{|//\\{\\{\\{|<!--\\{\\{\\{-->)\\n";	// original
-	};
-	mono.handler = function(w) {
-		var result = '';
-		var match;
-		var regExp;
-		
-		// test for "{{{ some text }}}"
-		regExp = /^.*}}}/im; 
-		match = regExp.exec(w.matchText);
-		if (match != null) {
-			return false; 	// it doesn't belong to this function.
-//		console.log('wrong handler!')		
-		}; 
-
-		// additional tests for highlighter text		
-		regExp = /^\{\{\{(.*)/im;
-		match = regExp.exec(w.matchText);
-		if (match != null) {
-			result = (expert) ? 'sh ' + match[1] : (guess) ? 'sh brush:text' : '';  // highlighter class info
-//		console.log('match: ', match, 'result: ', result, 'w', w);
-		}; 
-		regExp = /^\/\/\{\{\{(.*)/im;
-		match = regExp.exec(w.matchText);
-		if (match != null) {
-			result = (expert) ? 'sh ' + match[1] : (guess) ? 'sh brush:js' : '' ;  // highlighter class info
-//		console.log('match: ', match, 'result: ', result, 'w', w);
-		};
-		regExp = /^<!--\{\{\{(.*)-->/im;
-		match = regExp.exec(w.matchText);
-		if (match != null) {
-			result = (expert) ? 'sh ' + match[1] : (guess) ? 'sh brush:xml' : '' ;  // highlighter class info
-//		console.log('match: ', match, 'result: ', result, 'w', w);
-		};
-		regExp = /^\/\*\{\{\{(.*)\*\//im;
-		match = regExp.exec(w.matchText);
-		if (match != null) {
-			result = (expert) ? 'sh ' + match[1] : (guess) ? 'sh brush:css' : '';  // highlighter class info
-//		console.log('match: ', match, 'result: ', result, 'w', w);
-		}; 
-		
-		// delete whitespace at begin an end
-		result = result.replace(/^[ \t]+|[ \t]+$/mg, "");
-
-		// make identifier shorter because of additional info from above
-		match = w.matchText.slice(0,3);
-
-		switch(match) {
-		case "/*{": // CSS
-			this.lookaheadRegExp = /\/\*\{\{\{.*\*\/\n*((?:^[^\n]*\n)+?)(\n*^\f*\/\*\}\}\}\*\/$\n?)/mg;
-			break;
-		case "{{{": // monospaced block
-			this.lookaheadRegExp = /^\{\{\{.*\n((?:^[^\n]*\n)+?)(^\f*\}\}\}$\n?)/mg;
-			break;
-		case "//{": // plugin
-			this.lookaheadRegExp = /^\/\/\{\{\{.*\n\n*((?:^[^\n]*\n)+?)(\n*^\f*\/\/\}\}\}$\n?)/mg;
-			break;
-		case "<!-": //template
-			this.lookaheadRegExp = /<!--\{\{\{.*-->\n\n*((?:^[^\n]*\n)+?)(\n*^\f*<!--\}\}\}-->$\n?)/mg;
-			break;
-		default:
-			break;
-		};
-		
-		config.formatterHelpers.enclosedTextHelper.call(this, w, result);
-	} // mono.handler
 
 })(config.formatters); //# end of alias
 //}}}
-/***
-*Here the {{{<<highlightSyntax>>}}} is activated for this tiddler <<highlightSyntax>>
-***/
